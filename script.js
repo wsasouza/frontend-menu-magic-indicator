debe63f0f3beb16b32d172ca53385e5a440111fe
wsasouza <wsasouza@hotmail.com>
2021-09-22 23:28:45 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const marker = document.querySelector('#marker');
+const item = document.querySelectorAll('ul li a');
+
+function indicator(e) {
+  marker.style.left = e.offsetLeft + 'px';
+  marker.style.width = e.offsetWidth + 'px';
+}
+
+item.forEach(link => {
+  link.addEventListener('mousemove', (e) => {
+    indicator(e.target);
+  })
+})
